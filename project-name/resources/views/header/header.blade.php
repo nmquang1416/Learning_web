4f155c3fdcd76f3835911f21edf7fbdd5f7fcde9
Nguyễn Minh Quang <95731193+nmquang1416@users.noreply.github.com>
2024-12-16 22:33:15 +0700

    update header

--- project-name/resources/views/header/header.blade.php
+++ project-name/resources/views/header/header.blade.php
@@ -24,12 +24,12 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a href="#" class="navbar-brand">Eroge Learning</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item px-3 py-4">
                     <a href="#" class="nav-link" aria-current="page">Design</a>
                 </li>
@@ -46,10 +46,11 @@
                     <a href="#" class="nav-link" >All Coureses</a>
                 </li>
             </ul>
-            <form action="/" method="post">
+            <form class="d-flex" role="search">
                 @csrf
                 <i class="bi bi-search icon-search"></i>
-                <input type="text" class="form-control form-input " placeholder="Search anything...">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
         </div>
     </div>
